0562dd406604e56dc8908606e7e812c994509d9d
ZhangZfy <1043683248@qq.com>
2026-04-28 08:10:15 +0000

    Deploying to gh-pages from @ ZhangZfy/zhangzfy.github.io@447689e053d8196c8508d6ab761d353844926ba0 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "ZhangZfy/zhangzfy",
+    "data-repo": "ZhangZfy/zhangzfy.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
